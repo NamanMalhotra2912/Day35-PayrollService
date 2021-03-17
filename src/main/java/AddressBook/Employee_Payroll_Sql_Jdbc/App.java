71516d6edf06840d03332810891d3ab90f1069fc
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-03-17 15:35:14 +0530

    Employee Payroll using Jdbc

---
+++ src/main/java/AddressBook/Employee_Payroll_Sql_Jdbc/App.java
@@ -0,0 +1,13 @@
+package AddressBook.Employee_Payroll_Sql_Jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/AddressBook/Employee_Payroll_Sql_Jdbc/Welcome.java
@@ -0,0 +1,10 @@
+package AddressBook.Employee_Payroll_Sql_Jdbc;
+
+public class Welcome {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Employee payroll service ");
+	}
+
+}
